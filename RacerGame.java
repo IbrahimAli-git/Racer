e7b211eb5929ca520482b0df6421e8bdaf9d018c
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-19 09:25:56 +0100

    Update RacerGame.java

--- RacerGame.java
+++ RacerGame.java
@@ -55,11 +55,20 @@ public class RacerGame extends Game {
 
     private void moveAll(){
         roadMarking.move(player.speed);
+        player.move();
     }
 
     @Override
     public void onTurn(int step) {
         moveAll();
         drawScene();
+    }
+
+    @Override
+    public void onKeyPress(Key key) {
+        switch (key){
+            case RIGHT: player.setDirection(Direction.RIGHT); break;
+            case LEFT: player.setDirection(Direction.LEFT); break;
+        }
     }
 }
