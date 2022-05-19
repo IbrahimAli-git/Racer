64ab0f1c928493627de7fa5e6b4b5ba28806b5b7
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-19 10:24:14 +0100

    Update RoadManager.java

--- RoadManager.java
+++ RoadManager.java
@@ -6,4 +6,11 @@ public class RoadManager {
     public static final int LEFT_BORDER = RacerGame.ROADSIDE_WIDTH;
     public static final int RIGHT_BORDER = RacerGame.WIDTH - LEFT_BORDER;
 
+    private RoadObject createRoadObject(RoadObjectType roadObject, int x, int y){
+        if (roadObject == RoadObjectType.SPIKE){
+            return new Spike(x, y);
+        } else {
+            return null;
+        }
+    }
 }
