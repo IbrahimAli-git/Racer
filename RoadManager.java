badbecbcca240d46fddac0ab87989e211dec06fa
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-19 09:35:05 +0100

    Add files via upload

---
+++ RoadManager.java
@@ -0,0 +1,9 @@
+package com.codegym.games.racer.road;
+
+import com.codegym.games.racer.RacerGame;
+
+public class RoadManager {
+    public static final int LEFT_BORDER = RacerGame.ROADSIDE_WIDTH;
+    public static final int RIGHT_BORDER = RacerGame.WIDTH - LEFT_BORDER;
+
+}
