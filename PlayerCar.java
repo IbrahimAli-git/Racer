78a8441e8fede1ce7e328f34680a02102bd1ed84
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-19 09:26:26 +0100

    Update PlayerCar.java

--- PlayerCar.java
+++ PlayerCar.java
@@ -3,8 +3,24 @@ package com.codegym.games.racer;
 public class PlayerCar extends GameObject {
     private static int playerCarHeight = ShapeMatrix.PLAYER.length;
     public int speed = 1;
+    private Direction direction;
 
     public PlayerCar() {
         super(RacerGame.WIDTH/2+2, RacerGame.HEIGHT-playerCarHeight-1, ShapeMatrix.PLAYER);
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public void move(){
+        switch (direction){
+            case LEFT: x--; break;
+            case RIGHT: x++; break;
+        }
     }
 }
