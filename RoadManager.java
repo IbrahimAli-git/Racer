5ee2a5b41ac723df7202a89c2dceb7c6f5513d7e
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-20 09:23:32 +0100

    Update RoadManager.java

--- RoadManager.java
+++ RoadManager.java
@@ -1,87 +1,37 @@
-package com.codegym.games.racer.road;
+package com.codegym.games.racer;
 
-import com.codegym.engine.cell.Game;
-import com.codegym.games.racer.PlayerCar;
-import com.codegym.games.racer.RacerGame;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import com.codegym.games.racer.road.RoadManager;
 
-public class RoadManager {
-    public static final int LEFT_BORDER = RacerGame.ROADSIDE_WIDTH;
-    public static final int RIGHT_BORDER = RacerGame.WIDTH - LEFT_BORDER;
-    private static final int FIRST_LANE_POSITION = 16;
-    private static final int FOURTH_LANE_POSITION = 44;
-    private List<RoadObject> items = new ArrayList<>();
+public class PlayerCar extends GameObject {
+    private static int playerCarHeight = ShapeMatrix.PLAYER.length;
+    public int speed = 1;
+    private Direction direction;
 
+    public PlayerCar() {
+        super(RacerGame.WIDTH/2+2, RacerGame.HEIGHT-playerCarHeight-1, ShapeMatrix.PLAYER);
+    }
 
-    private RoadObject createRoadObject(RoadObjectType roadObject, int x, int y){
-        if (roadObject == RoadObjectType.SPIKE){
-            return new Spike(x, y);
-        } else {
-            return null;
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public void move(){
+        if (x < RoadManager.LEFT_BORDER){
+            x = RoadManager.LEFT_BORDER;
+        } else if(x > RoadManager.RIGHT_BORDER-width){
+            x = RoadManager.RIGHT_BORDER-width;
+        }
+        switch (direction){
+            case LEFT: x--; break;
+            case RIGHT: x++; break;
         }
     }
 
-    private void addRoadObject(RoadObjectType type, Game game){
-        int x = game.getRandomNumber(FIRST_LANE_POSITION, FOURTH_LANE_POSITION);
-        int y = -1 * RoadObject.getHeight(type);
-        RoadObject object = createRoadObject(type, x, y);
-        if (object != null) items.add(object);
-    }
-
-    public void draw(Game game){
-        for (RoadObject item : items) {
-            item.draw(game);
-        }
-    }
-
-    public void move(int boost){
-        for (RoadObject item : items) {
-            item.move(boost+ item.speed);
-        }
-        deletePassedItems();
-    }
-
-    public boolean checkCrash(PlayerCar car){
-        boolean isCrashable = false;
-        for (RoadObject item : items) {
-            if (item.isCollision(car)){
-                isCrashable = true;
-            }
-        }
-        return isCrashable;
-    }
-
-    private boolean spikeExists(){
-        boolean isContainingASpike = false;
-        for (RoadObject item : items) {
-            if (item instanceof Spike){
-                isContainingASpike = true;
-            }
-        }
-        return isContainingASpike;
-    }
-
-    private void generateSpike(Game game){
-        int i = game.getRandomNumber(100);
-        if (i < 10 && !spikeExists()){
-            addRoadObject(RoadObjectType.SPIKE, game);
-        }
-    }
-
-    public void generateNewRoadObjects(Game game){
-        generateSpike(game);
-    }
-
-    private void deletePassedItems(){
-        Iterator<RoadObject> iterator = items.listIterator();
-        while (iterator.hasNext()){
-            RoadObject roadObject = iterator.next();
-            if (roadObject.y >= RacerGame.HEIGHT){
-                iterator.remove();
-            }
-        }
-
+    public void stop(){
+        matrix = ShapeMatrix.PLAYER_DEAD;
     }
 }
